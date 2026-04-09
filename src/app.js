793f200ca091475586095152908bb33a21f7c741
zaid-6969 <bawasamohammedzaid@gmail.com>
2026-04-09 16:03:45 +0530

    just

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://hospital-management-frontend-rho.vercel.app/",
     credentials: true,
   }),
 );
