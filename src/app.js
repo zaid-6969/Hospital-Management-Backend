44520c86646a43ac3db857637d61b5807ec479ae
zaid-6969 <bawasamohammedzaid@gmail.com>
2026-04-10 12:28:35 +0530

    sdfa

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 import express, { json } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import authRoutes from "../router/auth.routes.js";
-import medicalRoutes from "../router/medical.routes.js";
-import availabilityRoutes from "../router/availability.routes.js";
-import appointmentRoutes from "../router/appointment.routes.js";
-import doctorRoutes from "../router/doctor.routes.js"
-import patientDetailsRoutes from "../router/patientDetails.routes.js";
+import authRoutes from "./router/auth.routes.js";
+import medicalRoutes from "./router/medical.routes.js";
+import availabilityRoutes from "./router/availability.routes.js";
+import appointmentRoutes from "./router/appointment.routes.js";
+import doctorRoutes from "./router/doctor.routes.js";
+import patientDetailsRoutes from "./router/patientDetails.routes.js";
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: CORS_ORIGIN,
     credentials: true,
   }),
 );
@@ -28,7 +28,7 @@ app.use("/api/v1/availability", availabilityRoutes);
 
 app.use("/api/v1/appointments", appointmentRoutes);
 
-app.use("/api/v1/doctors", doctorRoutes)
+app.use("/api/v1/doctors", doctorRoutes);
 
 app.use("/api/v1/patient-details", patientDetailsRoutes);
 
